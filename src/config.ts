d043816d7899cfff0b28b00236421c376690c357
xdimension <xdim222@gmail.com>
2024-10-19 04:13:06 +0000

    add config files

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// import { config } from "dotenv"
+// config()
+
+export default {
+  mainAppUrl: 'http://localhost:3333',
+  contractAddr: '0xd425f95801397b0D4A5D535E88a8d6F4935eC1eB',
+  ticketPrice: '0.0001'
+}
